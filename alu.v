3dd9759a35ee5053849dff0dd149d59b89d821b6
r3t2 <u16978199gou@163.com>
2026-03-11 20:06:36 +0800

    finish ALU

--- alu.v
+++ alu.v
@@ -12,7 +12,7 @@ module alu(A, B, ALUOp, C, Zero);
    always @( * ) begin
       case ( ALUOp )
       `ALUOp_lui:C=B;
-      `ALUOp_add:C=A+B;
+      `ALUOp_add:C=A+B; 
       `ALUOp_sub:C=A-B;  //and beq
       `ALUOp_xor:C=A^B;
       `ALUOp_or:C=A|B;
@@ -20,6 +20,14 @@ module alu(A, B, ALUOp, C, Zero);
       `ALUOp_sll:C=A<<B;
       `ALUOp_srl:C=A>>B;
       `ALUOp_sra:C=A>>>B;
+      `ALUOp_beq: C = A - B; // Zero = 1 作为 branch flag, b系列指令C的取值和条件真值相反
+      `ALUOp_bne: C = (A == B) ? 32'b1 : 32'b0; // 明确一点写明 32 位
+      `ALUOp_blt: C = (A >= B) ? 32'b1 : 32'b0;
+      `ALUOp_bge: C = (A <= B) ? 32'b1 : 32'b0;
+      `ALUOp_bltu: C = ($unsigned(A) >= $unsigned(B)) ? 32'b1 : 32'b0;
+      `ALUOp_bgeu: C = ($unsigned(A) <= $unsigned(B)) ? 32'b1 : 32'b0;
+      `ALUOp_slt: C = ($signed(A) < $signed(B)) ? 32'b1 : 32'b0;
+      `ALUOp_sltu: C = ($unsigned(A) < $unsigned(B)) ? 32'b1 : 32'b0;
       default: C=A;
       endcase
    end // end always
@@ -27,4 +35,3 @@ module alu(A, B, ALUOp, C, Zero);
    assign Zero = (C == 32'b0);  
 
 endmodule
-    
