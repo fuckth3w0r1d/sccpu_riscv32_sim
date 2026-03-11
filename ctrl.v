79f1fd59855a0cd650441d972e2d723142d599a0
r3t2 <u16978199gou@163.com>
2026-03-11 21:28:59 +0800

    finish CTRL

--- ctrl.v
+++ ctrl.v
@@ -34,6 +34,8 @@ module ctrl(Op, Funct7, Funct3, Zero,
     wire i_sll  = rtype& ~Funct7[6]&~Funct7[5]&~Funct7[4]&~Funct7[3]&~Funct7[2]&~Funct7[1]&~Funct7[0]&~Funct3[2]&~Funct3[1]& Funct3[0]; // sll 0000000 001
     wire i_srl  = rtype& ~Funct7[6]&~Funct7[5]&~Funct7[4]&~Funct7[3]&~Funct7[2]&~Funct7[1]&~Funct7[0]& Funct3[2]&~Funct3[1]& Funct3[0]; // srl 0000000 101
     wire i_sra  = rtype& ~Funct7[6]& Funct7[5]&~Funct7[4]&~Funct7[3]&~Funct7[2]&~Funct7[1]&~Funct7[0]& Funct3[2]&~Funct3[1]& Funct3[0]; // sra 0100000 101
+    wire i_slt = rtype& ~Funct7[6] & ~Funct7[5] & ~Funct7[4] & ~Funct7[3] & ~Funct7[2] & ~Funct7[1] & ~Funct7[0] & ~Funct3[2] & Funct3[1] & ~Funct3[0]; // slt 0000000 010
+    wire i_sltu = rtype& ~Funct7[6] & ~Funct7[5] & ~Funct7[4] & ~Funct7[3] & ~Funct7[2] & ~Funct7[1] & ~Funct7[0] & ~Funct3[2] & Funct3[1] & Funct3[0]; // sltu 0000000 011
 
  // i format load 0000011
     wire itype_l  = ~Op[6]&~Op[5]&~Op[4]&~Op[3]&~Op[2]&Op[1]&Op[0]; //0000011
@@ -42,6 +44,14 @@ module ctrl(Op, Funct7, Funct3, Zero,
   // i format 0010011
     wire itype_r  = ~Op[6]&~Op[5]&Op[4]&~Op[3]&~Op[2]&Op[1]&Op[0]; //0010011
     wire i_addi  =  itype_r& ~Funct3[2]& ~Funct3[1]& ~Funct3[0]; // addi 000
+    wire i_andi = itype_r& Funct3[2] & Funct3[1] & Funct[0]; // andi 111
+    wire i_ori = itype_r& Funct3[2] & Funct3[1] & ~Funct[0]; // ori 110
+    wire i_xori = itype_r& Funct3[2] & ~Funct3[1] & ~Funct[0];// xori 100
+    wire i_srli = itype_r& ~Funct7[6] & ~Funct7[5] & ~Funct7[4] & ~Funct7[3] & ~Funct7[2] & ~Funct7[1] & ~Funct7[0] & Funct3[2] & ~Funct3[1] & Funct3[0];// srli 0000000 101
+    wire i_srai = itype_r& ~Funct7[6] & Funct7[5] & ~Funct7[4] & ~Funct7[3] & ~Funct7[2] & ~Funct7[1] & ~Funct7[0] & Funct3[2] & ~Funct3[1] & Funct3[0];// srai 0100000 101
+    wire i_slli = itype_r& ~Funct7[6] & ~Funct7[5] & ~Funct7[4] & ~Funct7[3] & ~Funct7[2] & ~Funct7[1] & ~Funct7[0] & ~Funct3[2] & ~Funct3[1] & Funct3[0]// slli 0000000 001
+    wire i_slti = itype_r& ~Funct3[2] & Funct3[1] & ~Funct[0];// slti 010
+    wire i_sltui = itype_r& ~Funct3[2] & Funct3[1] & Funct[0]// sltui 011
     	
   // s format 0100011
     wire stype  = ~Op[6]&Op[5]&~Op[4]&~Op[3]&~Op[2]&Op[1]&Op[0]; //0100011
@@ -50,14 +60,21 @@ module ctrl(Op, Funct7, Funct3, Zero,
   // sb format 1100011
     wire sbtype  = Op[6]&Op[5]&~Op[4]&~Op[3]&~Op[2]&Op[1]&Op[0];//1100011
     wire i_beq  = sbtype& ~Funct3[2]& ~Funct3[1]&~Funct3[0]; // beq 000
+    wire i_bne = sbtype& ~Funct3[2] & ~Funct3[1] & Funct3[0];// bne 001
+    wire i_blt = sbtype& Funct3[2] & ~Funct3[1] & ~Funct3[0];// blt 100
+    wire i_bge = sbtype& Funct3[2] & ~Funct3[1] & Funct3[0];// bge 101
+    wire i_bltu = sbtype& Funct3[2] & Funct3[1] & ~Funct3[0];// bltu 110
+    wire i_bgeu = sbtype& Funct3[2] & Funct3[1] & Funct3[0];// bgeu 111
 
-  // j format
+  // j format 1101111
     wire i_jal  = Op[6]& Op[5]&~Op[4]& Op[3]& Op[2]& Op[1]& Op[0];  // jal 1101111
+    // jalr 1100111
+    wire i_jalr = Op[6] & Op[5] & ~Op[4] & ~Op[3] & Op[2] & Op[1] & Op[0];
 
   // generate control signals
-  assign RegWrite   = rtype | itype_r | LUI | itype_l; // register write
+  assign RegWrite   = rtype | itype_r | LUI | itype_l | i_jal | i_jalr; // register write
   assign MemWrite   = stype;                           // memory write
-  assign ALUSrc     = itype_r | LUI | itype_l | stype;   // ALU B is from instruction immediate
+  assign ALUSrc     = itype_r | LUI | itype_l | stype | i_jalr; // jal 走NPC自带的加法运算   // ALU B is from instruction immediate
 
   // signed extension
   // EXT_CTRL_ITYPE_SHAMT 6'b100000
@@ -66,30 +83,30 @@ module ctrl(Op, Funct7, Funct3, Zero,
   // EXT_CTRL_BTYPE	      6'b000100
   // EXT_CTRL_UTYPE	      6'b000010
   // EXT_CTRL_JTYPE	      6'b000001
-  assign EXTOp[5]    = 0;
-  assign EXTOp[4]    = i_addi | itype_l;
+  assign EXTOp[5]    = i_slli | i_srai | i_slli;
+  assign EXTOp[4]    = i_addi | itype_l | i_andi | i_slti | i_sltui | i_ori | i_xori | i_jalr;
   assign EXTOp[3]    = stype; 
   assign EXTOp[2]    = sbtype; 
   assign EXTOp[1]    = LUI;
-  assign EXTOp[0]    = i_jal;         
+  assign EXTOp[0]    = i_jal;        
   
   // WDSel_FromALU 2'b00
   // WDSel_FromMEM 2'b01
   // WDSel_FromPC  2'b10 
-    assign WDSel[1] = i_jal;
+    assign WDSel[1] = i_jal | i_jalr;
     assign WDSel[0] = itype_l;
 
   // NPC_PLUS4   3'b000
   // NPC_BRANCH  3'b001
   // NPC_JUMP    3'b010
   // NPC_JALR	   3'b100
-    assign NPCOp[2] = 0;
+    assign NPCOp[2] = i_jalr;
     assign NPCOp[1] = i_jal;
     assign NPCOp[0] = sbtype & Zero;
 
 // ALUOp_nop 5'b00000
 // ALUOp_lui 5'b00001
-// ALUOp_add 5'b00011
+// ALUOp_add 5'b00011 jalr会使用加法运算，所以jalr也使用这个op码
 // ALUOp_sub 5'b00100
 // ALUOp_xor 5'b01100
 // ALUOp_or 5'b01101
@@ -97,10 +114,20 @@ module ctrl(Op, Funct7, Funct3, Zero,
 // ALUOp_sll 5'b01111
 // ALUOp_srl 5'b10000
 // ALUOp_sra 5'b10001
-    assign ALUOp[4] = i_srl | i_sra;
-    assign ALUOp[3] = i_and | i_or | i_sll | i_xor;
-    assign ALUOp[2] = i_and | i_or | i_sub | i_beq | i_sll | i_xor;
-    assign ALUOp[1] = i_addi | i_add | i_and | i_sll | itype_l | stype ;
-	  assign ALUOp[0] = i_addi | i_add | i_or | LUI | i_sll | i_sra | itype_l | stype ;
+
+// `define ALUOp_auipc 5'b00010
+// `define ALUOp_bne 5'b00101
+// `define ALUOp_blt 5'b00110
+// `define ALUOp_bge 5'b00111
+// `define ALUOp_bltu 5'b01000
+// `define ALUOp_bgeu 5'b01001
+// `define ALUOp_slt 5'b01010
+// `define ALUOp_sltu 5'b01011
+
+    assign ALUOp[4] = i_srl | i_sra | i_srai | i_srli;
+    assign ALUOp[3] = i_and | i_or | i_sll | i_xor | i_slt | i_sltu | i_bltu | i_bgeu | i_slti | i_sltui | i_andi | i_xori | i_ori | i_slli;
+    assign ALUOp[2] = i_and | i_andi | i_or | i_ori | i_sub | i_beq | i_sll | i_slli | i_xor | i_xori | i_bne | i_blt | i_bge;
+    assign ALUOp[1] = i_addi | i_add | i_and | i_andi | i_sll | i_slli | itype_l | stype | i_blt | i_bge | i_slt | i_slti | i_sltu | i_sltui | i_jalr;
+	  assign ALUOp[0] = i_addi | i_add | i_or | i_ori | LUI | i_sll | i_slli | i_sra | i_srai | itype_l | stype | i_bne | i_bge | i_bgeu | i_sltu | i_sltui | i_jalr;
 
 endmodule
